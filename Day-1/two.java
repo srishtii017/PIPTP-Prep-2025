2fbede37e7e4dd95e4aea483a42a0a6c1330864e
Srishti Gupta <132117020+srishtii017@users.noreply.github.com>
2025-07-30 11:05:06 +0530

    Create two.java

---
+++ Day-1/two.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class two {
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        int a=1;
+        while(a<5){
+            a=a+2;
+            
+        }
+        System.out.print(a);
+    }
+}
+//loop will execute two times
