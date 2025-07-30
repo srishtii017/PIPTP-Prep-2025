9a28f701420a1ecf60265eb715e46f4c0db56001
Srishti Gupta <132117020+srishtii017@users.noreply.github.com>
2025-07-30 11:06:09 +0530

    Create fourth.java

---
+++ Day-1/fourth.java
@@ -0,0 +1,16 @@
+public class fourth {
+    public static void main(String[] args) {
+        int a=3
+        int b=3;
+        if(a==b){
+            if(1^1){
+                a=1;
+
+            }
+            else{
+                b=2;
+            }
+        }
+        System.out.print(a+b);
+    }
+}
